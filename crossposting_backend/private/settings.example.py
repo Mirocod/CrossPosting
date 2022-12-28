ec40a53d98fb8cded1b13ece08201dfbf22a2a38
Artur Galyamov <redex2000@gmail.com>
2022-12-28 18:57:42 +0500

    В настройки добавлен пример журналирования #27

--- crossposting_backend/private/settings.example.py
+++ crossposting_backend/private/settings.example.py
@@ -1,3 +1,6 @@
+from os import path
+from pathlib import Path
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 # SECURITY WARNING: keep the secret key used in production secret!
@@ -5,3 +8,30 @@ SECRET_KEY = 'django-insecure-aaaa'
 
 SALT = 'aaaaaaaa'
 ALLOWED_HOSTS = []
+
+LOG_DIR = path.join(Path(__file__).resolve().parent.parent.parent, 'logs/')
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'standard': {
+            'format': '%(asctime)s [%(levelname)s]- %(message)s'
+        }
+    },
+    'handlers': {
+        'file': {
+            'level': 'ERROR',
+            'class': 'logging.FileHandler',
+            'filename': LOG_DIR + 'application.log',
+            'formatter': 'standard'
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['file'],
+            'level': 'ERROR',
+            'propagate': True,
+        },
+    },
+}
